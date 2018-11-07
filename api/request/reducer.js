222ba15d703b9b8f2c9b9c3b1445ce1a08218c85
Intbizth <im_bonbonn@hotmail.com>
2018-11-07 16:25:11 +0700

    Update api/request/reducer.js

--- api/request/reducer.js
+++ api/request/reducer.js
@@ -1,10 +1,43 @@
-export default (entity, key) => {
+import extend from 'lodash/extend';
+
+export default (entity, key, options = {}) => {
+    options = extend({
+        ownLoading: false
+    }, options);
+
+    if (false === options.ownLoading) {
+        return (state = {}, action) => {
+            switch (action.type) {
+                case entity.SUCCESS:
+                    return {
+                        ...state,
+                        [key]: action.data,
+                    };
+                default:
+                    return state;
+            }
+        }
+    }
+
+    const loadingKey = `${key}isLoading`;
     return (state = {}, action) => {
         switch (action.type) {
+            case entity.REQUEST:
+                return {
+                    ...state,
+                    [loadingKey]: true
+                };
             case entity.SUCCESS:
                 return {
                     ...state,
                     [key]: action.data,
+                    [loadingKey]: false
+                };
+            case entity.FAILURE:
+            case entity.DISMISS:
+                return {
+                    ...state,
+                    [loadingKey]: false
                 };
             default:
                 return state;
